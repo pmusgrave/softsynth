1ecdaec4d35c5015b9877589f57c4671c4fe966d
pmusgrave <ps.musgrave@gmail.com>
2017-08-26 08:35:39 -0400

    first commit

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,108 @@
+#ifndef MAINCOMPONENT_H_INCLUDED
+#define MAINCOMPONENT_H_INCLUDED
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include <cmath>
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainContentComponent   : public AudioAppComponent,
+                               public Slider::Listener
+{
+public:
+    //==============================================================================
+    MainContentComponent()
+    :   currentSampleRate (0.0),
+        currentAngle (0.0),
+        angleDelta (0.0)
+    {
+        addAndMakeVisible (frequencySlider);
+        frequencySlider.setRange (50.0, 5000.0);
+        frequencySlider.setSkewFactorFromMidPoint (500.0); // [4]
+        frequencySlider.addListener (this);
+
+        setSize (800, 600);
+        setAudioChannels (0, 2);
+    }
+
+    ~MainContentComponent()
+    {
+        shutdownAudio();
+    }
+
+    //==============================================================================
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override
+    {
+        String message;
+        message << "Preparing to play audio...\n";
+        message << " samplesPerBlockExpected = " << samplesPerBlockExpected << "\n";
+        message << " sampleRate = " << sampleRate;
+        Logger::getCurrentLogger()->writeToLog (message);
+    }
+
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override
+    {
+        for (int channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel)
+        {
+            const float level = 0.125f;
+            float* const buffer = bufferToFill.buffer->getWritePointer (0, bufferToFill.startSample);
+
+            for (int sample = 0; sample < bufferToFill.numSamples; ++sample)
+            {
+                const float currentSample = (float) std::sin (currentAngle);
+                currentAngle += angleDelta;
+                buffer[sample] = currentSample * level;
+            }
+        }
+    }
+
+    void updateAngleDelta()
+    {
+        const double cyclesPerSample = frequencySlider.getValue() / currentSampleRate;
+        angleDelta = cyclesPerSample * 2.0 * double_Pi;
+    }
+
+    void releaseResources() override
+    {
+        Logger::getCurrentLogger()->writeToLog ("Releasing audio resources");
+    }
+
+    void sliderValueChanged (Slider* slider) override
+    {
+        if (slider == &frequencySlider)
+        {
+            if (currentSampleRate > 0.0)
+                updateAngleDelta();
+        }
+    }
+
+    //==============================================================================
+    void paint (Graphics& g) override
+    {
+        // (Our component is opaque, so we must completely fill the background with a solid colour)
+        //g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+        // You can add your drawing code here!
+    }
+
+    void resized() override
+    {
+        frequencySlider.setBounds (10, 10, getWidth() - 20, 20);
+    }
+
+
+private:
+    Random random;
+    Slider frequencySlider;
+    double currentSampleRate, currentAngle, angleDelta;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
+};
+
+Component* createMainContentComponent()     { return new MainContentComponent(); }
+
+
+#endif
